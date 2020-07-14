6abc2a4d994e712796bb3ecf3751b77c56dc4615
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-14 12:25:37 +0530

    Jul 14  12:25:37

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -6,7 +6,11 @@
 
 
 def fun_isfactor(f, n):
-	if (n % f == 0):
+	if (f == 0 and n != 0):
+		return False
+	elif (f == 0 and n == 0):
+		return True
+	elif (n % f == 0):
 		return True
 	else:
 		return False 
