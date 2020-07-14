9d4b278bde2fb29eb3931d148a928890e387415f
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-14 11:55:17 +0530

    Jul 14  11:55:16

--- 05-getinrange-Python/getinrange.py
+++ 05-getinrange-Python/getinrange.py
@@ -6,4 +6,18 @@
 
 def fun_getinrange(x, bound1, bound2):
 	# your code goes here
-	return -1
+	if (bound1 < bound2):
+		if (x < bound1):
+			return bound1
+		elif (x > bound2):
+			return bound2
+		else:
+			return x
+	else:
+		if (x < bound2):
+			return bound2
+		elif (x > bound1):
+			return bound1
+		else:
+			return x
+	
