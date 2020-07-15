2e42e71e2e3fa53973ad8838a267d648831a3b53
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-15 11:33:59 +0530

    Jul 15  11:33:59

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -6,7 +6,31 @@
 # are not legal values, return None, instead of crashing. 
 
 
-
+def printPascal(n) :
+    s1 = []
+    for line in range(0, n) :
+        s2 = []
+        for i in range(0, line + 1) : 
+            # a =  binomialCoeff(line, i)+" "+ end + ""
+            a =  binomialCoeff(line, i)
+            s2.append(a)
+        s1.append(s2)
+    return s1
+def binomialCoeff(n, k) : 
+    res = 1
+    if (k > n - k) : 
+        k = n - k 
+    for i in range(0 , k) : 
+        res = res * (n - i) 
+        res = res // (i + 1) 
+      
+    return res 
 
 def fun_pascaltrianglevalue(row, col):
-	return 1
+	m = printPascal(row + 1)
+	if (col <= (row+1)):
+	    return m[row][col]
+	else:
+	    return 0
+
+
