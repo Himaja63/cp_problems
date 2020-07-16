a556629531d204cf865e0d6ee2e96dbfa0611c2b
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-16 09:41:43 +0530

    Jul 16  09:41:43

--- 03-recursion-Python/test_solution.py
+++ 03-recursion-Python/test_solution.py
@@ -5,7 +5,7 @@ from recursion import get_fib
 import pytest
 
 
-@pytest.mark.parametrize("input_value, result",[(9, 34), (11, 89), (0, 0)])
+@pytest.mark.parametrize("input_value, result",[(10, 34), (12, 89), (0, 0)])
 def test_get_fib(input_value, result):    
     assert get_fib(input_value) == result
 
