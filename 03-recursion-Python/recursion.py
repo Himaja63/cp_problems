3e2822d212e90be46b3348b0c214ae3f8394a8ea
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-16 09:41:51 +0530

    Jul 16  09:41:51

--- 03-recursion-Python/recursion.py
+++ 03-recursion-Python/recursion.py
@@ -4,4 +4,12 @@ Your code should have the same input/output as the
 iterative code in the instructions."""
 
 def get_fib(position):
-    return -1
+    if position < 0:
+        return -1
+    elif (position == 0 or position == 1):
+        return 0
+    elif (position == 2):
+        return 1
+    else:
+        return get_fib(position - 1) + get_fib(position - 2)
+        
