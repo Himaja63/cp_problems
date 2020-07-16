4589295875c177744c20b01d12eead133b902b92
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-16 16:51:58 +0530

    Jul 16  16:51:58

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -11,5 +11,35 @@
 # assert(nthHappyNumber(7) == 31)
 
 
+def numSquareSum(n): 
+    squareSum = 0
+    while(n): 
+        squareSum += (n % 10) * (n % 10)
+        n = int(n / 10)
+    return squareSum
+
+def ishappynumber(n):
+	if (n > 0):
+		temp1 = n
+		temp2 = n
+		while(True):
+			temp1 = numSquareSum(temp1)
+			temp2 = numSquareSum(numSquareSum(temp2))
+			if (temp1 != temp2):
+				continue
+			else:
+				break
+		return (temp1 == 1)
+	else:
+		return False
+
 def fun_nth_happy_number(n):
-	return 0
+	c = 0
+	i = 1
+	while(c != n+1):
+		if(ishappynumber(i)):
+			c = c + 1
+			i = i + 1
+		else:
+			i = i + 1
+	return i - 1
