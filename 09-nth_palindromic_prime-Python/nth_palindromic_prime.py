45b1b2868cfdf68d6fc2f057ce82b2d57263c798
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-17 16:31:45 +0530

    Jul 17  16:31:45

--- 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
+++ 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
@@ -4,7 +4,42 @@
 # so 313 is an palindrome Prime. fun_nth_palindrome_prime(0) returns 2
 
 
+def palindrome(n):
+	sum = 0
+	temp = n
+	while(n != 0):
+		rem = n % 10
+		n = n // 10
+		sum = sum * 10 + rem
+	if (sum == temp):
+		return True
+	else:
+		return False
 
-
+def isPrime(num):
+    flag = 0
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                flag = 1
+                break
+            else: 
+                flag = 0
+    else: 
+       flag = 1
+       
+    if (flag == 0):
+        return True
+    else:
+        return False
+	
 def fun_nth_palindromic_prime(n):
-	return 0
+	c = 0
+	i = 1
+	while (c < n+1):
+		if (palindrome(i) and isPrime(i)):
+			c = c+1
+			i = i+1
+		else:
+			i = i+1
+	return i-1
