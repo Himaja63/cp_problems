450a5d24d5c347ad41cea088b36b67a5f19208be
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-17 15:51:08 +0530

    Jul 17  15:51:08

--- 04-vowelscount-Java/vowelscount.java
+++ 04-vowelscount-Java/vowelscount.java
@@ -10,7 +10,7 @@ class vowelscount {
 		int count = 0;
 		for(int j = 0; j < s.length(); j++){
 			char ch = s.charAt(j);
-			if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+			if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
 				count = count + 1;
 			}
 
