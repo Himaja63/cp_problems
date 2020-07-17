c1558219870770dfa35b613f2f10d00bc9ab73a7
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-17 11:59:11 +0530

    Jul 17  11:59:11

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -3,7 +3,39 @@
 # replace(s1, s2, s3) that returns a string equal to 
 # s1.replace(s2, s3), but again without calling s.replace().
 
-
+import itertools
 def fun_replace(s1, s2, s3):
-	return s1
+	res = ""
+	l1 = list(s1)
+	l2 = list(s2)
+	l3 = list(s3)
+	if(s2 in s1):
+		for i in range(len(l1)):
+			if (l1[i] == l2[0]):
+				if(l1[i:i+len(l2)] == l2):
+					x = i
+					y = 0
+					if(len(s3) <= len(s2)):
+						while(y < len(l2)):
+							if(y < len(l3)):
+								l1[x] = l3[y]
+							else:
+								l1[x] = "-"
+							x = x + 1
+							y = y + 1
+					else:
+						while(y < len(s2)-1):
+							l1[x] = l3[y]
+							x = x+1
+							y = y+1
+						l1[x] = l3[y:]
+
+		merged = list(itertools(*l1))
+		s = "".join(merged)
+		for i in s:
+			if(i != "-"):
+				res = res+i
+		return res
+	else:
+		return s1
 
