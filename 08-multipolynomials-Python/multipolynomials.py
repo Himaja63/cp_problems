840b030d9d03b7defb02034c105f80495d78ea84
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-19 00:16:11 +0530

    Jul 19  00:16:10

--- 08-multipolynomials-Python/multipolynomials.py
+++ 08-multipolynomials-Python/multipolynomials.py
@@ -7,4 +7,10 @@
 
 def multipolynomials(p1, p2):
 	# Your code goes here
-	pass
+	m = len(p1)
+	n = len(p2)
+	prod = [0] * (m+n-1)
+	for i in range(m):
+		for j in range(n):
+			prod[i+j] += p1[i]*p2[j]
+	return prod
