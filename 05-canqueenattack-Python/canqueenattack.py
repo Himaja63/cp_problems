1d793af8b5d63b1e20f73e57debc14ea18395f8a
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-20 16:51:02 +0530

    Jul 20  16:51:02

--- 05-canqueenattack-Python/canqueenattack.py
+++ 05-canqueenattack-Python/canqueenattack.py
@@ -3,6 +3,29 @@
 # whether the queen can attack the opponent or not. Note that the queen can attack in the same row, same column and 
 # diagonally.
 
+# def diag(qR, qC, oR, oC):
+# 	if (qC == qR+1 and oR == qC+1 and oC == oR+1):
+# 		return True
+# 	else:
+# 		return False
+
 def canqueenattack(qR, qC, oR, oC):
 	# Your code goes here
-	pass
+	row = []
+	col = []
+	l = []
+	for j in range(qC+1, 9):
+		col.append(j)
+	for i in range(qR+1, qR+1+len(col)):
+		row.append(i)
+	for k in range(len(row)):
+		t = (row[k], col[k])
+		l.append(t)
+	if (oR, oC) in l:
+		return True
+		
+	elif(qR == oR or qC == oC):
+		return True
+	else:
+		return False
+	
