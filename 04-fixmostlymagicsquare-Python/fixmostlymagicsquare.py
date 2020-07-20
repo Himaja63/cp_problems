5464070b0e2eaab0187120a10e968b09fff5200a
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-20 16:05:26 +0530

    Jul 20  16:05:26

--- 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
+++ 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
@@ -5,7 +5,80 @@
 # L, return a new list M such that M is the same as L, only with exactly one value changed, and M is a mostly magic 
 # square.
 
+def lookandsay(a):
+	# Your code goes here
+	l = []
+	l1 = []
+	if len(a) == 0:
+	    return l
+	else:
+	    i = 0
+	    j = 0
+	    c = 0
+	    l = a
+	    while(i < len(a) and j < len(l)):
+	        if(a[i] == l[j]):
+	            c = c+1
+	            j = j + 1
+	        else:
+	            t = (c,a[i])
+	            l1.append(t)
+	            i = j
+	            c = 0
+	    else:
+	        t = (c,a[i])
+	        l1.append(t)
+	    return l1
+def oddOneOut(l):
+    l1 = lookandsay(l)
+    for i in l1:
+	    if i[0] == 1:
+	        res = i[1]
+    return res
+
+def getIndex(l):
+    n = oddOneOut(l)
+    for i in range(len(l)):
+        if (l[i] == n):
+            res = i
+            break
+    return res
+    
+def sameNum(l):
+	n = oddOneOut(l)
+	arr = []
+	for i in range(len(l)):
+		if (l[i] != n):
+			arr.append(l[i])
+	return list(set(arr))
 
 def fixmostlymagicsquare(L):
-	pass
-	# Your code goes here
+	l1 = []	
+	l2 = []
+	arr = []
+	add = 0
+	for i in range(len(L)):
+		sum = 0
+		for j in range(len(L)):
+			sum = sum + L[i][j]
+		l1.append(sum)
+
+	for i in range(len(L)):
+		sum = 0
+		for j in range(len(L)):
+			sum = sum + L[j][i]
+		l2.append(sum)
+	row = getIndex(l1)
+	col = getIndex(l2)
+	num = sameNum(l1)[0]
+	for i in range(len(L)):
+		if i != col:
+			add = add + L[row][i]
+# 	print(num)
+# 	print(add)
+	ans = num - add
+# 	print(ans)
+	L[row][col] = ans
+	return L
+		
+	
