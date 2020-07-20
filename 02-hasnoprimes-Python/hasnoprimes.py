15ae69e77bec5399b3edafdf29fe43a6b4ec3e30
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-20 13:49:30 +0530

    Jul 20  13:49:30

--- 02-hasnoprimes-Python/hasnoprimes.py
+++ 02-hasnoprimes-Python/hasnoprimes.py
@@ -1,7 +1,33 @@
 # Write the function hasnoprimes(L) that takes a 2d list L of integers, 
 # and returns True if L does not contain any primes, and False otherwise.
 
+from itertools import chain 
+
+def isPrime(num):
+    flag = 0
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                flag = 1
+                break
+            else: 
+                flag = 0
+    else: 
+       flag = 1
+       
+    if (flag == 0):
+        return True
+    else:
+        return False
 
 def fun_hasnoprimes(l):
-	return True
-
+	flatten_list = list(chain.from_iterable(l))
+	flag = 0
+	for i in flatten_list:
+		if(isPrime(i)):
+			flag = 1
+	if(flag == 1):
+		return False
+	else:
+		return True
+	
