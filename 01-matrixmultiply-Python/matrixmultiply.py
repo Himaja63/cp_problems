2a14eb9507cba3137845df94af3c1796a357709f
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-21 09:36:37 +0530

    Jul 21  09:36:37

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -3,9 +3,45 @@
 # is the result of multiplying the two matrices. Return None if the 
 # two matrices cannot be multiplied for any reason.
 
+def isSquarematrix(m):
+    flag = 0
+    for i in range(len(m)):
+        for j in range(len(m)):
+            if (i != j):
+                if(len(m[i]) != len(m[j])):
+                    flag = 1
+                    break
+    if flag == 1:
+        return False
+    else:
+        return True
+
+def isCompatible(m1, m2):
+    flag = 0
+    if(isSquarematrix(m1) and isSquarematrix(m2)):
+        if(len(m1[0]) != len(m2)):
+            return False
+        else:
+            return True
+    else:
+        return False
 
 def fun_matrixmultiply(m1, m2):
-    return None
+    res = []
+    for i in range(len(m1)):
+        c = []
+        for j in range(len(m2[0])):
+            c.append(0)
+        res.append(c)
+    
+    if(isCompatible(m1.m2)):
+        for i in range(len(m1)):
+            for j in range(len(m2[0])):
+                for k in range(len(m2)):
+                    res[i][j] += m1[i][k]*m2[k][j]
+        return res
+    else:
+        return None
 
 
 
