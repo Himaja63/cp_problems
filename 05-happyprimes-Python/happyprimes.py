3d15fcc0e5fe6c6991504d29cd16604a2138d52e
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-23 17:21:10 +0530

    Jul 23  17:21:09

--- 05-happyprimes-Python/happyprimes.py
+++ 05-happyprimes-Python/happyprimes.py
@@ -9,6 +9,48 @@
 # Note: the autograder will grade each of the following functions, so they are required. 
 # However, they also are here specifically because they are just the right helper 
 # functions to make nthHappyNumber(n) easier to write!
+
+def numSquareSum(n): 
+    squareSum = 0
+    while(n): 
+        squareSum += (n % 10) * (n % 10)
+        n = int(n / 10)
+    return squareSum
+
+def ishappynumber(n):
+	if (n > 0):
+		temp1 = n
+		temp2 = n
+		while(True):
+			temp1 = numSquareSum(temp1)
+			temp2 = numSquareSum(numSquareSum(temp2))
+			if (temp1 != temp2):
+				continue
+			else:
+				break
+		return (temp1 == 1)
+	else:
+		return False
+
+def isPrime(num):
+    flag = 0
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                flag = 1
+                break
+            else: 
+                flag = 0
+    else: 
+       flag = 1
+       
+    if (flag == 0):
+        return True
+    else:
+        return False
+
 def ishappyprimenumber(n):
-    # Your code goes here
-    pass
+    if(ishappynumber(n) and isPrime(n)):
+        return True
+    else:
+        return False
