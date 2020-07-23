9bec8e3e2cd7cb9ed18c27fd4f9d98dc7d2a3e20
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-23 16:34:40 +0530

    Jul 23  16:34:39

--- 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
+++ 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
@@ -10,5 +10,52 @@
 
 import math
 
+def isPrime(num):
+    flag = 0
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                flag = 1
+                break
+            else: 
+                flag = 0
+    else: 
+       flag = 1
+       
+    if (flag == 0):
+        return True
+    else:
+        return False
+
+def isLeftTruncatablePrime(n):
+    flag = 0
+    s = str(n)
+    i = 0
+    if '0' in s:
+        return False
+    else:
+        while(i < len(s)):
+            if(isPrime(int(s[i:]))):
+                flag = 0
+                i = i+1
+            else:
+                flag = 1
+                break
+        if(flag == 0):
+            return True
+        else:
+            return False
+    
+
 def fun_nth_lefttruncatableprime(n):
-    return 1
+    c = 0
+    j = 1
+    while(c <= n):
+        if(isLeftTruncatablePrime(j)):
+            c = c+1
+            j = j+1
+        else:
+            j = j+1
+    return j-1
+
+    
