88232305ad37b391f34d5bbbf6cc55dea81c0d80
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-07-25 22:25:26 +0530

    Jul 25  22:25:26

--- 06-bstpractice-Java/BST.java
+++ 06-bstpractice-Java/BST.java
@@ -17,21 +17,61 @@ public class BST {
     }
 
     public void insert(int value) {
-    	// Your code goes here
-    }
+        // Your code goes here
+        Node node = this.root;
+        this.root=insert(node, value);
+        }
+        
 
-    private void insert(Node node, int value) {
+    private Node insert(Node node, int value) {
+        if(node == null) return new Node(value);
+        if(value<node.value) {
+            System.out.println("here1");
+            node.left=insert(node.left, value);
+        }else if(value>node.value) {
+            node.right=insert(node.right, value);
+        } else {
+            node.value=value;
+        }
+        return node;
     	// Your code goes here
     }
 
     public boolean search(int value) {
-    	// Your code goes here
-    	return false;
+        // Your code goes here
+        boolean x=search(this.root,value);
+        System.out.println("x" + x);
+        return x;
+    	 
     }
 
     private boolean search(Node current, int value) {
-    	// Your code goes here
-    	return false;
+        boolean x;
+       if(value >current.value) {
+            if(current.right ==null) {
+                
+                return false;
+            }
+            x =search(current.right, value);
+        }else if(value<current.value) {
+            
+            if(current.left == null) {
+                
+                return false;}
+            
+            x=search(current.left, value);
+        }else{
+            
+            return true;
+       } System.out.println("out");
+       return x;
     }
-
-}
+       public static void main(String[] args) {
+           BST obj = new BST(4);
+           obj.insert(2);
+           obj.insert(1);
+           obj.insert(3);
+           obj.insert(5);
+           System.out.println(obj.search(5));
+       }
+    }
